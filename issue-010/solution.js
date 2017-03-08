4898ff783e11642d128d5c4237307e713c37ef41
dessHub <deskip95@gmail.com>
2017-03-09 01:53:25 +0300

    solved drill 1.9a

--- issue-010/solution.js
+++ issue-010/solution.js
@@ -1,4 +1,21 @@
 // implement your solution here
-function evenOdd(a) {
+function evenOdd(arg) {
 
+  var diff = 0;
+  for(i=0; i<arg.length; i++){
+    var odd = 0;
+    var even = 0;
+   if (arg[i]%2 === 0){
+       even += arg[i];
+   }else if(arg[i]%2 !== 0){
+       odd += arg[i];
+   }
+  diff += even-odd;
+  }
+
+  return diff;
 }
+
+var a = [10, 5, 2];
+
+console.log(evenOdd(a));
